037f5753b1ec36779f83ec17c469fcf5c5a17672
Tiago Belintani <113792806+TiagoBelintani@users.noreply.github.com>
2025-08-12 12:40:03 -0700

    Create phyluce_assembly_match_contigs_to_probes_job.sh

---
+++ Jobs_Conf/phyluce_assembly_match_contigs_to_probes_job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -t 1:00:00
+#SBATCH -c 1
+
+
+module load miniconda/3-2023-09
+
+source $(conda info --base)/etc/profile.d/conda.sh
+source activate /home/tiagobelintani/miniconda3/envs/phyluce-1.7.3
+
+phyluce_assembly_match_contigs_to_probes \
+    --contigs /home/tiagobelintani/uce-treinamento/assembly/contigs \
+    --probes /home/tiagobelintani/uce-treinamento/probes/probes.fasta \
+    --output uce-resultados-busca
